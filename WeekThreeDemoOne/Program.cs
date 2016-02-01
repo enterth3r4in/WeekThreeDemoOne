f14d5b6fd79ea5ce947c8e498e2d56673efeb996
enterth3r4in <believe.the.rain@gmail.com>
2016-02-01 14:03:01 -0500

    InClassWork

--- WeekThreeDemoOne/Program.cs
+++ WeekThreeDemoOne/Program.cs
@@ -10,7 +10,33 @@ namespace WeekThreeDemoOne
     {
         static void Main(string[] args)
         {
+            int     ID = 123;
+            string firstName = "FIRST_NAME";
+            string  lastName = "LAST_NAME";
+            string  itemDescription = "ITEM_DESCRIPTION";
+            int     quantity = 0;
+            double  weight = 0.00;
+            decimal price = 0.00m;
+            decimal shippingConstantPerPound = 0.89m;
 
+            ID          = 185181;
+            firstName   = "Alexi";
+            lastName    = "Stukov";
+
+            Console.WriteLine("Customer Info");
+            Console.WriteLine("[ID, last, first]\n");
+            Console.WriteLine(ID + " " + lastName + ", " + firstName + "\n");
+
+            itemDescription = "Spinning Top";
+            quantity = 15;
+            weight = 1.27;
+            price = 3.76m;
+
+            Console.WriteLine("First Order: ");
+            Console.WriteLine("Order of " + quantity + " of " + "'"+itemDescription+"'");
+            Console.WriteLine("Approx Cost (before shipping): " + quantity * price);
+            Console.WriteLine("Shipping Cost: " + (decimal)weight * shippingConstantPerPound);
+            Console.WriteLine("Total Cost: " + ((quantity * price) + ((decimal)weight * shippingConstantPerPound)));
         }
     }
 }
